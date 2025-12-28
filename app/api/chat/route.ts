87c5981673c6a39f4bd4bd2a4b386d3039d5c61e
Dev Bachani <dev22603@gmail.com>
2025-12-28 16:47:54 +0200

    saves messages to db

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -9,7 +9,7 @@ import { randomUUID } from "crypto";
 import { pool } from "@/lib/db";
 import { success, error } from "@/lib/response";
 import { getConversation } from "@/repositories/conversationRepository";
-import { getConversationHistory } from "@/repositories";
+import { getConversationHistory, saveMessage } from "@/repositories";
 import { getChat } from "@/config/gemini";
 
 export async function POST(request: Request) {
@@ -27,17 +27,13 @@ export async function POST(request: Request) {
 		conversationId
 	);
 
-	// create a new conversation if the conversation id is null
-	// if the conversation id is not null then fetch the messages, how u will do that: fetch the conversation from database
-	// use conversationRepository.getConversation(conversationId)
-	// get a chat model from gemini.ts with or without history, whatever the case is
-	// get the reply, save it to the messages table and update the conversation (the time field)
-	// return the reply and conversation id
-
 	const history: GeminiHistoryMessage[] = convertToGeminiFormat(conversation);
 	const chat = getChat(history);
 	const response = await chat.sendMessage({ message: userMessage });
-
+	saveMessage(conversationId, userMessage, "user");
+	if (response.text) {
+		saveMessage(conversationId, response.text, "model");
+	}
 	return success<ChatData>(201, "message sent", {
 		message: response.text || "Chatbot error: no response received",
 		conversationId: conversationId,
